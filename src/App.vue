83f14969ee66bc6a94e8599c34d5c2783e9bc5ed
aSwanting <gab.damico@gmail.com>
2023-11-10 13:06:03 +0100

    add: vite + vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><AppMain></AppMain></div>
+</template>
+
+<script>
+import AppMain from "./components/AppMain.vue";
+export default {
+  components: {
+    AppMain,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>App Main</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
