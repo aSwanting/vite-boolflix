3431dda0bdefb1382ce18db86cbe0c7dafd93446
aSwanting <gab.damico@gmail.com>
2023-11-10 18:43:25 +0100

    milestone 2: search now includes tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,39 @@
 <template>
   <main>
-    <div>
-      <ul>
-        <li v-for="(movie, index) in store.movies">
-          <ul>
-            <li><span>Title: </span>{{ movie.title }}</li>
-            <li><span>Original Title: </span>{{ movie.original_title }}</li>
-            <li><span>Language: </span>{{ movie.original_language }}</li>
-            <li>
-              <img :src="getFlag(movie.original_language)" />
-            </li>
-            <li><span>Vote Average: </span>{{ movie.vote_average }}</li>
-          </ul>
-        </li>
-      </ul>
+    <div class="search-results">
+      <div class="movies">
+        <h3>Movies</h3>
+        <ul>
+          <li v-for="(movie, index) in store.movies">
+            <ul>
+              <li><span>Title: </span>{{ movie.title }}</li>
+              <li><span>Original Title: </span>{{ movie.original_title }}</li>
+              <li><span>Vote Average: </span>{{ movie.vote_average }}</li>
+              <li><span>Language: </span>{{ movie.original_language }}</li>
+              <li>
+                <img :src="getFlag(movie.original_language)" />
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div class="series">
+        <h3>Series</h3>
+        <ul>
+          <li v-for="(series, index) in store.series">
+            <ul>
+              <li><span>Title: </span>{{ series.name }}</li>
+              <li><span>Original Title: </span>{{ series.original_name }}</li>
+              <li><span>Vote Average: </span>{{ series.vote_average }}</li>
+              <li><span>Language: </span>{{ series.original_language }}</li>
+              <li>
+                <img :src="getFlag(series.original_language)" />
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
@@ -45,22 +65,41 @@ main {
   border-radius: 10px;
 }
 
-ul {
-  margin-bottom: 30px;
+.search-results {
+  display: flex;
+  gap: 20px;
 
-  li {
-    margin-bottom: 5px;
-  }
-  span {
-    font-weight: bold;
+  h3 {
+    text-align: center;
+    margin-bottom: 30px;
+    text-transform: uppercase;
   }
 
-  img {
-    display: block;
-    border: 1px solid rgb(200, 200, 200);
-    width: 50px;
-    aspect-ratio: 3 / 2;
-    object-fit: cover;
+  .movies,
+  .series {
+    padding: 20px;
+    border: 2px solid grey;
+    border-radius: 10px;
+    flex-basis: 50%;
+  }
+
+  ul {
+    margin-bottom: 50px;
+
+    li {
+      margin-bottom: 5px;
+    }
+    span {
+      font-weight: bold;
+    }
+
+    img {
+      display: block;
+      border: 1px solid rgb(200, 200, 200);
+      width: 50px;
+      aspect-ratio: 3 / 2;
+      object-fit: cover;
+    }
   }
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -3,22 +3,28 @@ import axios from "axios";
 
 const store = reactive({
   API_KEY: "e6c36204d368eb79057d546cfae14aba",
-  searchMoviesUrl: "https://api.themoviedb.org/3/search/movie",
+  endpoints: [
+    "https://api.themoviedb.org/3/search/movie",
+    "https://api.themoviedb.org/3/search/tv",
+  ],
   query: "",
   movies: [],
+  series: [],
 
   fetchData() {
-    axios
-      .get(this.searchMoviesUrl, {
+    const requests = this.endpoints.map((endpoint) =>
+      axios.get(endpoint, {
         params: {
           api_key: this.API_KEY,
           query: this.query,
         },
       })
-      .then((res) => {
-        this.movies = res.data.results;
-        console.log(this.movies);
-      });
+    );
+    axios.all(requests).then((res) => {
+      this.movies = res[0].data.results;
+      this.series = res[1].data.results;
+      console.log(this.movies, this.series);
+    });
   },
 });
 
