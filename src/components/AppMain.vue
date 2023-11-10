ca0b5c43bf28092664c074225a90bf319d6f04c3
aSwanting <gab.damico@gmail.com>
2023-11-10 17:57:21 +0100

    add: show country flags based on language

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,9 @@
             <li><span>Title: </span>{{ movie.title }}</li>
             <li><span>Original Title: </span>{{ movie.original_title }}</li>
             <li><span>Language: </span>{{ movie.original_language }}</li>
+            <li>
+              <img :src="getFlag(movie.original_language)" />
+            </li>
             <li><span>Vote Average: </span>{{ movie.vote_average }}</li>
           </ul>
         </li>
@@ -17,11 +20,19 @@
 
 <script>
 import store from "../store.js";
+import { getLanguage } from "language-flag-colors";
+
 export default {
   data() {
     return {
       store,
     };
+  },
+  methods: {
+    getFlag(lang) {
+      const countryCode = getLanguage(lang).countryCode.replace("ja", "jp");
+      return `https://flagcdn.com/w160/${countryCode}.png`;
+    },
   },
 };
 </script>
@@ -35,9 +46,21 @@ main {
 }
 
 ul {
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+
+  li {
+    margin-bottom: 5px;
+  }
   span {
     font-weight: bold;
+  }
+
+  img {
+    display: block;
+    border: 1px solid rgb(200, 200, 200);
+    width: 50px;
+    aspect-ratio: 3 / 2;
+    object-fit: cover;
   }
 }
 </style>
