36198131a84d0a69d363837af873ac22eace8d8c
aSwanting <gab.damico@gmail.com>
2023-11-10 14:49:29 +0100

    Milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>App Main</div>
+  <div>
+    <ul>
+      <li v-for="(movie, index) in store.movies">
+        <ul>
+          <li><span>Title: </span>{{ movie.title }}</li>
+          <li><span>Original Title: </span>{{ movie.original_title }}</li>
+          <li><span>Language: </span>{{ movie.original_language }}</li>
+          <li><span>Vote Average: </span>{{ movie.vote_average }}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import store from "../store.js";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin-bottom: 20px;
+
+  span {
+    font-weight: bold;
+  }
+}
+</style>
