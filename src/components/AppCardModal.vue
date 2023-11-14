4ea28aac5cd2de3cafd1622dbf35143907479a7d
aSwanting <gab.damico@gmail.com>
2023-11-14 12:29:46 +0100

    add: modal

---
+++ src/components/AppCardModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="modal">
+    <h1>info</h1>
+  </div>
+</template>
+
+<script>
+import store from "../store";
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  background-color: white;
+  width: 500px;
+  aspect-ratio: 3/2;
+}
+</style>
